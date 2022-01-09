70440ef792141ad4b656f9cbc59423fe6510fd4d
Jaipal <97381154+JaiGoutam@users.noreply.github.com>
2022-01-09 20:28:39 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 using namespace std;
-struct solutions
-{int missing(int nums[],int n)
-    {
-    int total=0;
-    int sum;
-        sum=n*(n+1)/2;
-        for(int i=0;i<n;i++)
-            total=total+nums[i];
-        return sum-total;
-    }};
-int main() {
-    solutions s;
-    int missing;
-    int n;
-    cin>>n;
-    int nums[n];
-    for(int i=0;i<n;i++)
-    cin>>nums[i];
-    missing=s.missing(nums,n);
-    cout<<missing;
-    return 0;}
+int maxLoot(int *hval, int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return hval[0];
+    if (n == 2)
+        return max(hval[0], hval[1]);
+    int dp[n];
+    dp[0] = hval[0];
+    dp[1] = max(hval[0], hval[1]);
+    for (int i = 2; i<n; i++)
+        dp[i] = max(hval[i]+dp[i-2], dp[i-1]);
+
+    return dp[n-1];
+}
+
+int main()
+{
+    int number;
+    cin>>number;
+    int nums[number];
+    for(int i=0;i<number;i++)
+        cin>>nums[i];
+    int n = sizeof(nums)/sizeof(nums[0]);
+    cout << "Maximum loot possible : "
+         << maxLoot(nums, n);
+    return 0;
+}
